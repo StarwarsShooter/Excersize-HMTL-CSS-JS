2093602ad922757ab4dcf439e6f677f440eddc2d
StarwarsShooter <61284683+StarwarsShooter@users.noreply.github.com>
2020-04-07 11:29:01 +0200

    Add project files.

---
+++ dieren/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dieren.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
