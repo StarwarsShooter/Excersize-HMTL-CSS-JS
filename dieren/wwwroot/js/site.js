56ab992affbd2c1d8555c20cb1fae61e0fe118d0
StarwarsShooter <61284683+StarwarsShooter@users.noreply.github.com>
2020-05-29 19:22:35 +0200

    Start javascript working

--- dieren/wwwroot/js/site.js
+++ dieren/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function OnReady() {
+    console.log('Pagina geladen, DOM klaar voor gebruik.');
+}
+$(document).ready(OnReady);
+function focusFunction() {
+    $("#name").focus("#fit");
+}
+function blurFunction() {
+    $("#name").blur("#fit");
+}
